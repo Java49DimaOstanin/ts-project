0e46f939171faf0f4a045151681e8679b58d95dc
DimaOstanin <xnadab@gmail.com>
2023-01-04 21:23:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+// for(let i = 0; i< 3 ; i++){
+//     setTimeout(() => console.log(i))
+// }
+// let a  = 10;
+// console.log(`a = ${a}`)
+// variable
+let hello = "world";
+const vasya = "vasya";
+const b = 10;
+let c;
+c = "20";
+function getRandomNumber(min, max) {
+    return Math.floor(min + Math.random() * (max - min + 1));
+}
+// hw 30
+function shiftCipher(str, shift = 1) {
+    //todo
+    // for each lower case letter (a-z ) 
+    // you should perform code ASCII on given shift 
+    // shift should be in cicle of lower case letter
+    // code ASCII  'z' + 2 = b ASCII
+    // examples : 
+    // shiftCipher ("abz.", 3) => "dec."
+    return "";
+}
+function shiftDeCipher(str, shift = 1) {
+    return " ";
+}
+//# sourceMappingURL=app.js.map
